cec86b6100872e47f458b8ad3609013e000c8467
GuilhermeFreitass <guilhermeaqf@gmail.com>
2025-12-06 21:49:19 -0300

    feat: add TaskPriority and TaskStatus enums, enhance task model with new fields and create project model

---
+++ taskflow-api/prisma/migrations/20251207004225_add_task_fields_and_project/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "TaskPriority" AS ENUM ('Alta', 'Media', 'Normal');
+
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('Pendente', 'EmProgresso', 'Concluido', 'Cancelado');
+
+-- AlterTable
+ALTER TABLE "task" ADD COLUMN     "dueDate" TIMESTAMP(3),
+ADD COLUMN     "isImportant" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "priority" "TaskPriority" NOT NULL DEFAULT 'Normal',
+ADD COLUMN     "projectId" INTEGER,
+ADD COLUMN     "status" "TaskStatus" NOT NULL DEFAULT 'Pendente',
+ALTER COLUMN "updatedAt" DROP DEFAULT;
+
+-- CreateTable
+CREATE TABLE "project" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "project_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "task" ADD CONSTRAINT "task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
